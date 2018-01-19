d442f6caf4af527f9dfbe99f97b36ede5f5f1c6b
mamed <barapoi5@gmail.com>
2018-01-19 20:29:38 +0800

    karet lentur

--- tugas4.cpp
+++ tugas4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class ban
 	public:
-		void bahan(karet);
+		void bahan(string karet);
 		string merk;
 		double harga;
 		
@@ -15,4 +15,10 @@ int main () {
 	
 	tubless.merk ="yokohama";
 	tubless.harga = 13900000;
+	
+	cout<<"merk yang dipakai "<< (tubless.merk)<<endl;
+	cout<<"Harga modal nya "<< (tubless.harga)<<endl;
+	
+	tubless.bahan("karet nya terbuat dari gatah");
+}
 	
